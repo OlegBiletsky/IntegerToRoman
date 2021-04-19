60fd07a1e4aa426ad5844db36c37c9cf01b6dd9a
OlegBiletsky <ol.biletsky@gmail.com>
2021-04-19 12:45:04 +0300

    toRoman add live mode

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let intToRoman = function(num) {
     let result = '';
+    // arrays with Roman Numbers
     let arrayRomanThousands = ['M', 'MM', 'MMM'];
     let arrayRomanHundreds = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
     let arrayRomanTens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
     let arrayRomanUnit = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-
+    //schedule of numbers into digits
     let thousands = Math.floor( num / 1000 );
     let hundreds = Math.floor( num / 100 ) % 10;
     let tens = Math.floor( num / 10 ) % 10;
@@ -34,10 +35,29 @@ let intToRoman = function(num) {
             break;
         }
     }
-
     return result;
 };
 
 
 
-intToRoman(1);
+let live__input = document.querySelector('.live__input');
+live__input.addEventListener('input', convert)
+
+let live__result = document.querySelector('.live__result');
+
+function convert() {
+    if ( typeof(+live__input.value) === 'number' && 
+        (+live__input.value) >= 1 &&  
+        (+live__input.value)<=3999 ) 
+    {
+        live__result.innerText = intToRoman(live__input.value);
+    }
+    else {
+        live__result.innerText = 'Incorrect value';
+    }
+}
+
+function OnBlur() {
+    live__input.value = 2021;
+    live__result.innerText = intToRoman(live__input.value);
+}
